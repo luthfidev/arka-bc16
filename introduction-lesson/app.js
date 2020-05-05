ec3f1db5c4df683825d8a066cca92536289569f2
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-05 18:11:44 +0700

    pembahasan

--- introduction-lesson/app.js
+++ introduction-lesson/app.js
@@ -1,5 +1,17 @@
 const data = require('./src/identity')
 const validate = require('./src/validate')
 
+
 console.log(data)
+
+//pembahasan revisi
+/* for (key in data){
+    console.log(data[key])
+} */
+
 validate()
+
+/* //validate username
+console.log('validate username: ',username('arka'))
+//validate password
+console.log('validate password: ',password('arkadem')) */
--- introduction-lesson/src/validate.js
+++ introduction-lesson/src/validate.js
@@ -18,4 +18,14 @@ function validation(){
 
 }
 
+//pembahasan
+/* const validate = {
+    username: (string='') => {
+      return RegExp(/[A-Za-z0-9]{5,}/g).test(string)
+    },
+    password: (string) => {
+      return string.length === 8 ? true : false
+    }
+  } */
+
 module.exports = validation
